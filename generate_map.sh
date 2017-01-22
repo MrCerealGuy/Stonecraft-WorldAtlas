c23344cfa56dcca593ed9f7caa4dd8d1fa16571e
MrCerealGuy <mrcerealguy@gmx.de>
2017-01-22 17:32:58 +0100

    Updated license information

--- generate_map.sh
+++ generate_map.sh
@@ -1,20 +1,21 @@
 #!/bin/bash -e
 
-# Generate from a Stonecraft world an online map
-# Copyright (C) 2017  Andreas "MrCerealGuy" Zahnleiter
+# Generate an online map from a Stonecraft/Minetest world
 #
-#  This program is free software: you can redistribute it and/or modify
+# Copyright (C) 2017 Andreas "MrCerealGuy" Zahnleiter
+#
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # Made by Andreas 'MrCerealGuy' Zahnleiter
 # 2017-01-22: mrcerealguy: initial release
